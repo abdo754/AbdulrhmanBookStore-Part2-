a070372f004bcf5464d01069acf523d508838c10
abdo754 <W0786364@myscc.ca>
2023-11-06 21:48:12 -0500

    update

---
+++ AbdulrhmanBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using AbdulrhmanBooks.DataAccess.Repository.IRepository;
+using AbdulrhmanBooks.Models;
+using AbdulrhmanBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbdulrhmanBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db; // Correct the field name
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db; // Correct the field name
+        }
+    }
+}
---
+++ AbdulrhmanBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbdulrhmanBooks.DataAccess.Repository.IRepository
+{
+    class ICategoryRepository
+    {
+    }
+}
