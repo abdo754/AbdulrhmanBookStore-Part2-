746596c2756b831a331851fb9126e51f3ec7397d
abdo754 <W0786364@myscc.ca>
2023-11-06 22:00:33 -0500

    done editing the CATgory class

--- AbdulrhmanBooks.DataAccess/Repository/CategoryRepository.cs
+++ AbdulrhmanBooks.DataAccess/Repository/CategoryRepository.cs
@@ -2,21 +2,26 @@
 using AbdulrhmanBooks.Models;
 using AbdulrhmanBookStore.DataAccess.Data;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AbdulrhmanBooks.DataAccess.Repository
 {
     public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
-        private readonly ApplicationDbContext _db; // Correct the field name
+        private readonly ApplicationDbContext _db;
+
         public CategoryRepository(ApplicationDbContext db) : base(db)
         {
-            _db = db; // Correct the field name
+            _db = db;
         }
+
         public void Update(Category category)
         {
-            throw new NotImplementedException();
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+            }
         }
     }
 }
