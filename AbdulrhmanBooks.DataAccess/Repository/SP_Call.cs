27605df664372afaaa6fccf0b01f0516486a1af5
abdo754 <W0786364@myscc.ca>
2023-11-09 21:23:34 -0500

    update

--- AbdulrhmanBooks.DataAccess/Repository/SP_Call.cs
+++ AbdulrhmanBooks.DataAccess/Repository/SP_Call.cs
@@ -1,7 +1,7 @@
 ﻿using AbdulrhmanBooks.DataAccess.Repository.IRepository;
 using AbdulrhmanBookStore.DataAccess.Data;
 using Dapper;
-using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,12 +12,10 @@ namespace AbdulrhmanBooks.DataAccess.Repository
     public class SP_Call : ISP_Call
     {
         private readonly ApplicationDbContext _db;
-        private static string ConnectionString = "";
 
         public SP_Call(ApplicationDbContext db)
         {
             _db = db;
-            ConnectionString = db.Database.GetDbConnection().ConnectionString;
         }
 
         public void Dispose()
@@ -27,28 +25,31 @@ namespace AbdulrhmanBooks.DataAccess.Repository
 
         public void Execute(string procedurename, DynamicParameters param = null)
         {
-            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            // Use the existing connection from DbContext
+            using (var dbConnection = _db.Database.GetDbConnection())
             {
-                sqlCon.Open(); // Correct the method name
-                sqlCon.Execute(procedurename, param, commandType: CommandType.StoredProcedure); // Correct the method name
+                dbConnection.Open();
+                dbConnection.Execute(procedurename, param, commandType: CommandType.StoredProcedure);
             }
         }
 
         public IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null)
         {
-            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            // Use the existing connection from DbContext
+            using (var dbConnection = _db.Database.GetDbConnection())
             {
-                sqlCon.Open(); // Correct the method name
-                return sqlCon.Query<T>(procedurename, param, commandType: CommandType.StoredProcedure); // Correct the method name
+                dbConnection.Open();
+                return dbConnection.Query<T>(procedurename, param, commandType: CommandType.StoredProcedure);
             }
         }
 
         public Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null)
         {
-            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            // Use the existing connection from DbContext
+            using (var dbConnection = _db.Database.GetDbConnection())
             {
-                sqlCon.Open(); // Correct the method name
-                var result = SqlMapper.QueryMultiple(sqlCon, procedurename, param, commandType: CommandType.StoredProcedure); // Correct the method name
+                dbConnection.Open();
+                var result = SqlMapper.QueryMultiple(dbConnection, procedurename, param, commandType: CommandType.StoredProcedure);
                 var item1 = result.Read<T1>().ToList();
                 var item2 = result.Read<T2>().ToList();
 
@@ -57,25 +58,27 @@ namespace AbdulrhmanBooks.DataAccess.Repository
                     return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(item1, item2);
                 }
             }
-            return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(new List<T1>(), new List<T2>()); // Correct the initialization
+            return new Tuple<IEnumerable<T1>, IEnumerable<T2>>(new List<T1>(), new List<T2>());
         }
 
         public T OneRecord<T>(string procedurename, DynamicParameters param = null)
         {
-            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            // Use the existing connection from DbContext
+            using (var dbConnection = _db.Database.GetDbConnection())
             {
-                sqlCon.Open(); // Correct the method name
-                var value = sqlCon.Query<T>(procedurename, param, commandType: CommandType.StoredProcedure); // Correct the method name
+                dbConnection.Open();
+                var value = dbConnection.Query<T>(procedurename, param, commandType: CommandType.StoredProcedure);
                 return value.FirstOrDefault();
             }
         }
 
         public T Single<T>(string procedurename, DynamicParameters param = null)
         {
-            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            // Use the existing connection from DbContext
+            using (var dbConnection = _db.Database.GetDbConnection())
             {
-                sqlCon.Open(); // Correct the method name
-                return sqlCon.ExecuteScalar<T>(procedurename, param, commandType: CommandType.StoredProcedure); // Correct the method name
+                dbConnection.Open();
+                return dbConnection.ExecuteScalar<T>(procedurename, param, commandType: CommandType.StoredProcedure);
             }
         }
     }
