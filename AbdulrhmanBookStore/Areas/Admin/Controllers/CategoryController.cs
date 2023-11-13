f35fd8053f0d65492efcf687aadf8c1b64498153
abdo754 <W0786364@myscc.ca>
2023-11-13 00:18:18 -0500

    redo

---
+++ AbdulrhmanBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbdulrhmanBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController: Controller
+    {
+        private readonly IUnitofWork unitofWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult index()
+        {
+            return View();
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+        }
+        #endregion
+    }
+}
