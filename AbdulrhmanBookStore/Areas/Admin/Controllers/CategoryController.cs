a199a1c957e7ed98590537c4236bb578773d287c
abdo754 <W0786364@myscc.ca>
2023-11-13 00:31:34 -0500

    still not webpage working

--- AbdulrhmanBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ AbdulrhmanBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AbdulrhmanBooks.DataAccess.Repository.IRepository; // Ensure the correct namespace is used for IUnitOfWork
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,23 +8,26 @@ using System.Threading.Tasks;
 namespace AbdulrhmanBookStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class CategoryController: Controller
+    public class CategoryController : Controller
     {
-        private readonly IUnitofWork unitofWork;
+        private readonly IUnitOfWork _unitOfWork; // Ensure the field name matches the constructor parameter
 
         public CategoryController(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork; // Correctly assign the parameter to the field
         }
-        public IActionResult index()
+
+        public IActionResult Index() // Method names in C# are typically PascalCase
         {
             return View();
         }
+
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj }); // Return the data as JSON
         }
         #endregion
     }
